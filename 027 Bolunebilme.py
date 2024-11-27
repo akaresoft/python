11ad0eb5a7322686a738dcfb91092440a52e7225
Akaresoft Yazılım <58430984+akaresoft@users.noreply.github.com>
2024-11-27 16:53:06 +0300

    Add files via upload

---
+++ 027 Bolunebilme.py
@@ -0,0 +1,6 @@
+s=int(input("Bir sayı giriniz:"))
+
+if (s%3==0 and s%7==0):
+    print(s," sayısı üçe ve yediye bölünür")
+else:
+    print("üçe ve yediye bölünmez")
---
+++ 028 prim.py
@@ -0,0 +1,6 @@
+# 10.000 altı prim yok
+# 10.000<satis<25.000 ise prim 1500 TL
+# 25.000<=satis<50.000 ise prim 5.000 TL
+# 50.000<=satis<75.000 ise prim 8500 TL
+# 75.000<=satis<200.000 ise prim 15.000 TL
+# 200 bin ve üstü için prim satışın %10'u olacaktır. 
