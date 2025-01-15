652617de7b8836e89c190be3cfa3072fd179cf49
AkareSoft <58430984+akaresoft@users.noreply.github.com>
2025-01-15 16:53:04 +0300

    Add files via upload

---
+++ python_fonksiyonlar/002 fonksiyon_adsoyad.py
@@ -0,0 +1,10 @@
+def birlestir(adi,soyadi):
+    print("Sayın "+adi+" "+soyadi+" Hoşgeldiniz")
+
+# birlestir() fonksiyonu adi ve soyadi
+# değerlerini alarak ekrana birleştirerek yazmaktadır.
+# Hiç bir değer geri döndürmemektedir
+
+isim="Oğuzhan"
+soyisim="Taş"
+birlestir(isim,soyisim)
---
+++ python_fonksiyonlar/003 fonksiyon_kare.py
@@ -0,0 +1,9 @@
+def kareal(x):
+    return (x*x)
+
+# şimdi kareal fonsksiyonunu çağıralım
+# bu fonksiyon bir parametre alıyor
+# ve geriye sayının karesini döndürüyor
+
+a= int(input("Karesi alınacak sayıyı giriniz:"))
+print("Kare sonucu:", kareal(a))
